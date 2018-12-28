5552a12289f0402bf250c5bfd7a3ef21a22b7391
wyd <pk209654321@163.com>
2018-12-28 09:14:13 +0800

    2018-12-27 1835

--- src/main/scala/testBury/TempRunBury.scala
+++ src/main/scala/testBury/TempRunBury.scala
@@ -1,22 +1,23 @@
-package localAction
+package testBury
 
-import com.alibaba.fastjson.JSON
 import org.apache.commons.lang3.StringUtils
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.hive.HiveContext
-import scalaUtil.{DateScalaUtil, LocalOrLine}
-import sparkAction.BuryMainFunction
+import scalaUtil.LocalOrLine
+import sparkAction.BuryLogin
+import sparkAction.BuryMainFunction.cleanCommonFunction
 
 /**
-  * ClassName LocalScala
+  * ClassName TempRunBury
   * Description TODO
   * Author lenovo
-  * Date 2018/12/27 17:53
+  * Date 2018/12/28 9:01
   **/
-object LocalScala {
+object TempRunBury {
   def main(args: Array[String]): Unit = {
     val local: Boolean = LocalOrLine.judgeLocal()
+
     var sparkConf: SparkConf = new SparkConf().setAppName("BuryMainFunction")
     if (local) {
       System.setProperty("HADOOP_USER_NAME", "wangyd")
@@ -25,16 +26,12 @@ object LocalScala {
     val sc: SparkContext = new SparkContext(sparkConf)
     sc.setLogLevel("WARN")
     val hc: HiveContext = new HiveContext(sc)
-    //输入路径
-    val realPath = args(0)
+    val realPath =""
     val file: RDD[String] = sc.textFile(realPath, 1)
     val filterBlank: RDD[String] = file.filter(line => {
       //过滤为空的和有ip但是post传递为空的
       StringUtils.isNotBlank(line) && StringUtils.isNotBlank(line.split("&")(0))
     })
-    val value= filterBlank.map(BuryMainFunction.cleanCommonFunction)
-    //输出路径
-    value.saveAsTextFile(args(1))
+    val map: RDD[BuryLogin] = filterBlank.map(cleanCommonFunction).cache()
   }
-
 }
